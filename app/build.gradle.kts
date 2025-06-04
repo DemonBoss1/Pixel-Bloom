d229311b77b11d5220f79be703e7bb3e2b9d02cd
DemonBoss1 <121947683+DemonBoss1@users.noreply.github.com>
2025-06-04 13:28:00 +0300

    retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,18 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
 
     id("kotlin-kapt")
 }
+
+val localProperties = Properties().apply {
+    load(File(rootProject.rootDir, "local.properties").reader())
+}
+
+val apiKey = localProperties.getProperty("apiKey") ?: error("API key not found!")
+val secretKey = localProperties.getProperty("secretKey") ?: error("API key not found!")
 
 android {
     namespace = "com.empire_mammoth.pixelbloom"
@@ -17,6 +26,9 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        buildConfigField("String", "apiKey", "\"$apiKey\"")
+        buildConfigField("String", "secretKey", "\"$secretKey\"")
     }
 
     buildTypes {
@@ -34,6 +46,10 @@ android {
     }
     kotlinOptions {
         jvmTarget = "11"
+    }
+
+    buildFeatures {
+        buildConfig = true
     }
 }
 
--- app/src/main/java/com/empire_mammoth/pixelbloom/di/ApiModule.kt
+++ app/src/main/java/com/empire_mammoth/pixelbloom/di/ApiModule.kt
@@ -1,9 +1,16 @@
 package com.empire_mammoth.pixelbloom.di
 
+import com.empire_mammoth.pixelbloom.BuildConfig
+import dagger.Module
 import dagger.Provides
 import okhttp3.Interceptor
+import okhttp3.OkHttp
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
+@Module
 class ApiModule {
     @Provides
     @Singleton
@@ -11,10 +18,30 @@ class ApiModule {
         return Interceptor { chain ->
             val original = chain.request()
             val requestBuilder = original.newBuilder()
-                .header("X-Key", "Key $apiKey")
-                .header("X-Secret", "Secret $secretKey")
+                .header("X-Key", "Key ${BuildConfig.apiKey}")
+                .header("X-Secret", "Secret ${BuildConfig.secretKey}")
             val request = requestBuilder.build()
             chain.proceed(request)
         }
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(
+        authInterceptor: Interceptor
+    ): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(authInterceptor)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://api-key.fusionbrain.ai/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
     }
 }
