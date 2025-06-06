d65d7333e21ebd54ff5e02afd31c6bbb9ba94ec3
DemonBoss1 <121947683+DemonBoss1@users.noreply.github.com>
2025-06-06 12:40:52 +0300

    getGenerationStatus

--- app/src/main/java/com/empire_mammoth/pixelbloom/data/api/GenerateApiService.kt
+++ app/src/main/java/com/empire_mammoth/pixelbloom/data/api/GenerateApiService.kt
@@ -1,5 +1,6 @@
 package com.empire_mammoth.pixelbloom.data.api
 
+import com.empire_mammoth.pixelbloom.data.model.GenerationResponseWithResult
 import com.empire_mammoth.pixelbloom.data.model.GenerationStatusResponse
 import com.empire_mammoth.pixelbloom.data.model.Pipeline
 import okhttp3.RequestBody
@@ -9,15 +10,21 @@ import retrofit2.http.GET
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
+import retrofit2.http.Path
 
 interface GenerateApiService {
     @GET("key/api/v1/pipelines")
     suspend fun getPipeline(): List<Pipeline>
 
     @Multipart
-    @POST("key/api/v1/pipeline/run") // Replace with the correct endpoint
+    @POST("key/api/v1/pipeline/run")
     suspend fun generateImage(
         @Part("pipeline_id") pipelineId: RequestBody,
         @Part("params") params: RequestBody
     ): GenerationStatusResponse
+
+    @GET("key/api/v1/pipeline/status/{request_id}")
+    suspend fun getGenerationStatus(
+        @Path("request_id") requestId: String
+    ): GenerationResponseWithResult
 }
--- app/src/main/java/com/empire_mammoth/pixelbloom/presentation/MainActivity.kt
+++ app/src/main/java/com/empire_mammoth/pixelbloom/presentation/MainActivity.kt
@@ -7,6 +7,7 @@ import com.empire_mammoth.pixelbloom.data.api.GenerateApiService
 import com.empire_mammoth.pixelbloom.data.model.GenerationStatusResponse
 import com.empire_mammoth.pixelbloom.databinding.ActivityMainBinding
 import com.empire_mammoth.pixelbloom.di.DaggerAppComponent
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.RequestBody.Companion.toRequestBody
@@ -32,9 +33,8 @@ class MainActivity : AppCompatActivity() {
                 val pipeline = generateApiService.getPipeline()
                 val generationStatusResponse = generateImage(pipeline[0].id, "Sun in sky")
                 runOnUiThread {
-                    binding?.textViewMain?.text = generationStatusResponse.uuid
+                    binding?.textViewMain?.text = generationStatusResponse?.first()
                 }
-                // Работа с pipeline
             } catch (e: Exception) {
                 // Обработка ошибки
             }
@@ -43,15 +43,24 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private suspend fun generateImage(pipelineId: String, prompt: String): GenerationStatusResponse {
+    private suspend fun generateImage(pipelineId: String, prompt: String): List<String>? {
 
         val paramsJson = """
             {"type": "GENERATE", "numImages": 1, "width": 1024, "height": 1024, "generateParams": {"query": "$prompt"}}
-        """.trimIndent() // Use a multi-line string for readability
+        """.trimIndent()
 
         val pipelineIdBody = pipelineId.toRequestBody("text/plain".toMediaTypeOrNull())
         val paramsBody = paramsJson.toRequestBody("application/json".toMediaTypeOrNull())
 
-        return generateApiService.generateImage(pipelineIdBody, paramsBody)
+        val generationStatusResponse = generateApiService.generateImage(pipelineIdBody, paramsBody)
+
+        var attempts = 60
+        while(attempts>0){
+            val status = generateApiService.getGenerationStatus(generationStatusResponse.uuid)
+            if(status.status == "DONE") return status.result?.files
+            attempts -= 1
+            delay(5000)
+        }
+        return null
     }
 }
